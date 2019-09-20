23dbb034a97b5ca9632f2a8cd9a9984f273e6389
Changfeng Liu <steven24liu@gmail.com>
2019-09-19 17:51:29 -0700

    add protoChainFactory

---
+++ test/helper/protoChainLoader.js
@@ -0,0 +1,18 @@
+const protobuf = require("protobufjs");
+protobuf.common('google/protobuf/descriptor.proto', {})
+
+module.exports = async () => {
+    sgn = await protobuf.load(`${__dirname}/../../contracts/lib/data/proto/sgn.proto`);
+
+    const PenaltyRequest = sgn.lookupType("sgn.PenaltyRequest");
+    const PenaltyInfo = sgn.lookupType("sgn.PenaltyInfo");
+    const Penalty = sgn.lookupType("sgn.Penalty");
+    const AccountAmtPair = sgn.lookupType("sgn.AccountAmtPair");
+
+    return {
+        PenaltyRequest,
+        PenaltyInfo,
+        Penalty,
+        AccountAmtPair
+    }
+}
