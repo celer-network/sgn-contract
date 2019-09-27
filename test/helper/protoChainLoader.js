689afe7aae50748e4f01768c092ea68a86604d12
Changfeng Liu <steven24liu@gmail.com>
2019-09-27 11:26:59 -0700

    add test helpers

---
+++ test/helper/protoChainLoader.js
@@ -0,0 +1,20 @@
+const protobuf = require("protobufjs");
+protobuf.common("google/protobuf/descriptor.proto", {})
+
+module.exports = async () => {
+    sgn = await protobuf.load(`${__dirname}/../../contracts/lib/data/proto/sgn.proto`);
+
+    const PenaltyRequest = sgn.lookupType("sgn.PenaltyRequest");
+    const RewardRequest = sgn.lookupType("sgn.RewardRequest");
+    const Penalty = sgn.lookupType("sgn.Penalty");
+    const Reward = sgn.lookupType("sgn.Reward");
+    const AccountAmtPair = sgn.lookupType("sgn.AccountAmtPair");
+
+    return {
+        PenaltyRequest,
+        RewardRequest,
+        Penalty,
+        Reward,
+        AccountAmtPair
+    }
+}
---
+++ test/helper/utilities.js
@@ -0,0 +1,14 @@
+function uint2bytes(x) {
+  if (x < 0) {
+    throw "Input number is less than 0!";
+  }
+
+  var bytes = [];
+  do {
+    bytes.push(x & 255);
+    x = x >> 8;
+  } while (x)
+  return bytes.reverse();
+}
+
+module.exports = { uint2bytes }
