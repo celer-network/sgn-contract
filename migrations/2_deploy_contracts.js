5ee90470c598e73b6ef01aac71ea57bec04fa167
Xuefeng Zhu <xzhu15@illinois.edu>
2020-06-07 21:27:02 -0700

    set proper parameter for deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,15 @@ const ERC20ExampleToken = artifacts.require('ERC20ExampleToken');
 const DPoS = artifacts.require('DPoS');
 const SGN = artifacts.require('SGN');
 
+const governProposalDeposit = 1000000000000000000;
+const governVoteTimeout = 2;
+const blameTimeout = 0;
+const minValidatorNum = 0;
+const maxValidatorNum = 10;
+const minStakeInPool = 1000000000000000000;
+const increaseRateWaitTime = 2;
+const dposGoLiveTimeout = 0;
+
 module.exports = function (deployer, network, accounts) {
   return deployer
     .deploy(ERC20ExampleToken)
@@ -16,14 +25,14 @@ module.exports = function (deployer, network, accounts) {
       return deployer.deploy(
         DPoS,
         ERC20ExampleToken.address,
-        0,
-        0,
-        0,
-        0,
-        11,
-        0,
-        0,
-        0
+        governProposalDeposit,
+        governVoteTimeout,
+        blameTimeout,
+        minValidatorNum,
+        maxValidatorNum,
+        minStakeInPool,
+        increaseRateWaitTime,
+        dposGoLiveTimeout
       );
     })
     .then((dpos) => {
