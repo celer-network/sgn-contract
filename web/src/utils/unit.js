a1430dce5c9a4299d9b1de69765d12ef9bf6cae2
Xuefeng Zhu <xzhu15@illinois.edu>
2020-09-17 23:14:19 -0700

    Improve candidate table

--- web/src/utils/unit.js
+++ web/src/utils/unit.js
@@ -6,31 +6,35 @@ import './network';
 export const CELR = 'CELR';
 
 export const getUnitByAddress = (supportedTokens, address) => {
-    const token = _.find(
-        supportedTokens,
-        supportedToken => supportedToken.address === address
-    );
+  const token = _.find(
+    supportedTokens,
+    supportedToken => supportedToken.address === address
+  );
 
-    if (!token) {
-        return '';
-    }
-    return token.symbol;
+  if (!token) {
+    return '';
+  }
+  return token.symbol;
 };
 
-export const formatCurrencyValue = (value, unit) => {
-    if (!value) {
-        return;
-    }
+export const formatCurrencyValue = (value, unit, showDecimal) => {
+  if (!value) {
+    return;
+  }
 
-    const num = _.toNumber(value);
+  const num = _.toNumber(value);
 
-    if (num < 100000) {
-        return `${value} wei`;
-    }
+  if (num < 100000) {
+    return `${value} wei`;
+  }
 
-    return `${web3.utils.fromWei(value).split('.')[0]} ${unit}`;
+  if (showDecimal) {
+    return `${web3.utils.fromWei(value)} ${unit}`;
+  }
+
+  return `${web3.utils.fromWei(value).split('.')[0]} ${unit}`;
 };
 
-export const formatCelrValue = value => {
-    return formatCurrencyValue(value, CELR);
+export const formatCelrValue = (value, showDecimal) => {
+  return formatCurrencyValue(value, CELR, showDecimal);
 };
