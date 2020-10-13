6f09772b9b81d8a38895209f97adedac663994ce
Xiaozhou Li <xli@celer.network>
2020-10-12 17:15:43 -0700

    dpc-01,02

--- contracts/lib/DPoSCommon.sol
+++ contracts/lib/DPoSCommon.sol
@@ -7,9 +7,9 @@ pragma solidity ^0.5.0;
 library DPoSCommon {
     // Unbonded: not a validator and not responsible for previous validator behaviors if any.
     //   Delegators now are free to withdraw stakes (directly).
-    // Bonded: active validator. Delegators has to wait for slashTimeout to withdraw stakes.
+    // Bonded: active validator. Delegators have to wait for slashTimeout to withdraw stakes.
     // Unbonding: transitional status from Bonded to Unbonded. Candidate has lost the right of
-    //   validator but is still responsible for any misbehavors done during being validator.
+    //   validator but is still responsible for any misbehaviour done during being validator.
     //   Delegators should wait until candidate's unbondTime to freely withdraw stakes.
     enum CandidateStatus { Unbonded, Bonded, Unbonding }
 }
