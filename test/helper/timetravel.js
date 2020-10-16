b86e85f5fd627c86b83d21ee74f619b6780638af
Xuefeng Zhu <xzhu15@illinois.edu>
2020-10-16 02:53:54 -0700

    measure slash gas

--- test/helper/timetravel.js
+++ test/helper/timetravel.js
@@ -1,60 +1,60 @@
 // base on https://medium.com/edgefund/time-travelling-truffle-tests-f581c1964687
 
 const advanceTimeAndBlock = async time => {
-    await advanceTime(time);
-    await advanceOneBlock();
+  await advanceTime(time);
+  await advanceOneBlock();
 
-    return Promise.resolve(web3.eth.getBlock('latest'));
+  return Promise.resolve(web3.eth.getBlock('latest'));
 };
 
 const advanceTime = time => {
-    return new Promise((resolve, reject) => {
-        web3.currentProvider.send(
-            {
-                jsonrpc: '2.0',
-                method: 'evm_increaseTime',
-                params: [time],
-                id: new Date().getTime()
-            },
-            (err, result) => {
-                if (err) {
-                    return reject(err);
-                }
-                return resolve(result);
-            }
-        );
-    });
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: '2.0',
+        method: 'evm_increaseTime',
+        params: [time],
+        id: new Date().getTime()
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      }
+    );
+  });
 };
 
 const advanceOneBlock = () => {
-    return new Promise((resolve, reject) => {
-        web3.currentProvider.send(
-            {
-                jsonrpc: '2.0',
-                method: 'evm_mine',
-                id: new Date().getTime()
-            },
-            (err, result) => {
-                if (err) {
-                    return reject(err);
-                }
-                const newBlockHash = web3.eth.getBlock('latest').hash;
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: '2.0',
+        method: 'evm_mine',
+        id: new Date().getTime()
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        const newBlockHash = web3.eth.getBlock('latest').hash;
 
-                return resolve(newBlockHash);
-            }
-        );
-    });
+        return resolve(newBlockHash);
+      }
+    );
+  });
 };
 
 async function advanceBlocks(number) {
-    for (let i = 0; i < number; i++) {
-        await advanceOneBlock();
-    }
+  for (let i = 0; i < number; i++) {
+    await advanceOneBlock();
+  }
 }
 
 module.exports = {
-    advanceTime,
-    advanceOneBlock,
-    advanceBlocks,
-    advanceTimeAndBlock
+  advanceTime,
+  advanceOneBlock,
+  advanceBlocks,
+  advanceTimeAndBlock
 };
--- test/helper/utilities.js
+++ test/helper/utilities.js
@@ -1,14 +1,38 @@
 function uint2bytes(x) {
-    if (x < 0) {
-        throw new Error('Input number is less than 0!');
-    }
+  if (x < 0) {
+    throw new Error('Input number is less than 0!');
+  }
 
-    const bytes = [];
-    do {
-        bytes.push(x & 255);
-        x = x >> 8;
-    } while (x);
-    return bytes.reverse();
+  const bytes = [];
+  do {
+    bytes.push(x & 255);
+    x = x >> 8;
+  } while (x);
+  return bytes.reverse();
 }
 
-module.exports = { uint2bytes };
+async function getDeployGasUsed(instance) {
+  const receipt = await web3.eth.getTransactionReceipt(
+    instance.transactionHash
+  );
+  return receipt.gasUsed;
+}
+
+function getCallGasUsed(tx) {
+  return tx.receipt.gasUsed;
+}
+
+async function mineBlockUntil(deadline, sendAccount) {
+  let block = await web3.eth.getBlock('latest');
+  while (block.number <= deadline) {
+    await web3.eth.sendTransaction({ from: sendAccount }); // dummy block consumer
+    block = await web3.eth.getBlock('latest');
+  }
+}
+
+module.exports = {
+  uint2bytes,
+  getDeployGasUsed,
+  getCallGasUsed,
+  mineBlockUntil
+};
