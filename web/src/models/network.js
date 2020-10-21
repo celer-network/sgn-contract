2e346ea218edac2ce21d3586e660f4d4bf28f125
Michael Zhou <zhoumotongxue008@gmail.com>
2020-10-21 16:32:45 +0800

    Update default gateway URL

--- web/src/models/network.js
+++ web/src/models/network.js
@@ -1,26 +1,26 @@
 const SETTING_KEY = 'setting';
 
 export default {
-    namespace: 'network',
+  namespace: 'network',
 
-    state: {
-        setting: JSON.parse(localStorage.getItem(SETTING_KEY)) || {
-            gateway: `${window.location.protocol}//${window.location.hostname}:1317`
-        }
-    },
-
-    effects: {
-        *saveSetting({ payload }, { put }) {
-            const { setting } = payload;
-            localStorage.setItem(SETTING_KEY, JSON.stringify(setting));
-
-            yield put({ payload, type: 'save' });
-        }
-    },
-
-    reducers: {
-        save(state, action) {
-            return { ...state, ...action.payload };
-        }
+  state: {
+    setting: JSON.parse(localStorage.getItem(SETTING_KEY)) || {
+      gateway: `${window.location.protocol}//${window.location.hostname}/gateway`
     }
+  },
+
+  effects: {
+    *saveSetting({ payload }, { put }) {
+      const { setting } = payload;
+      localStorage.setItem(SETTING_KEY, JSON.stringify(setting));
+
+      yield put({ payload, type: 'save' });
+    }
+  },
+
+  reducers: {
+    save(state, action) {
+      return { ...state, ...action.payload };
+    }
+  }
 };
